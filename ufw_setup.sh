7e0d74217d962a27f827d8ba198f66a296dc3dc9
imranZERO <imran.leave@gmail.com>
2024-09-27 16:30:54 +0600

    added ufw setup script

---
+++ ufw_setup.sh
@@ -0,0 +1,10 @@
+# https://christitus.com/linux-security-mistakes/
+sudo ufw limit 22/tcp
+sudo ufw allow 80/tcp
+sudo ufw allow 443/tcp
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+sudo ufw enable
+
+# localsend - https://forums.linuxmint.com/viewtopic.php?t=408601
+sudo ufw allow 53317/tcp
