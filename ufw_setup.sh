37dcfabe17cb8fc2a623bfc95d8efff91550ae4d
imranZERO <imran.leave@gmail.com>
2025-07-12 11:35:12 +0600

    update

--- ufw_setup.sh
+++ ufw_setup.sh
@@ -4,7 +4,6 @@ sudo ufw allow 80/tcp
 sudo ufw allow 443/tcp
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
-sudo ufw enable
 
 # Local web servers
 sudo ufw allow 8000
@@ -12,13 +11,18 @@ sudo ufw allow 8000
 # localsend - https://forums.linuxmint.com/viewtopic.php?t=408601
 sudo ufw allow 53317/tcp
 
-# Unified Remote server
-sudo ufw allow 9512/tcp
-sudo ufw allow 9512/udp
+# Jellyfin Server
+sudo ufw allow 8096
+
+# Navidrome Server
+sudo ufw allow 4533
 
 # KDE Connect Server
 sudo ufw allow 1714:1764/tcp
 sudo ufw allow 1714:1764/udp
 
-# Navidrome Server
-sudo ufw allow 4533
+# Unified Remote server
+sudo ufw allow 9512/tcp
+sudo ufw allow 9512/udp
+
+sudo ufw enable
