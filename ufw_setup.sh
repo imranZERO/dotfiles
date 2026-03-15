ebe7155d87d29a27fd76a96fe25500505742894d
imranZERO <imran.leave@gmail.com>
2026-03-15 07:58:22 +0600

    add ufw entries

--- ufw_setup.sh
+++ ufw_setup.sh
@@ -8,6 +8,14 @@ sudo ufw default allow outgoing
 # Local web servers
 sudo ufw allow 8000
 
+# Syncthing
+sudo ufw allow 22000/tcp
+sudo ufw allow 22000/udp
+sudo ufw allow 21027/udp
+
+# iperf3
+sudo ufw allow 5201
+
 # localsend - https://forums.linuxmint.com/viewtopic.php?t=408601
 sudo ufw allow 53317/tcp
 
@@ -24,5 +32,10 @@ sudo ufw allow 1714:1764/udp
 # Unified Remote server
 sudo ufw allow 9512/tcp
 sudo ufw allow 9512/udp
+
+# Waydroid
+sudo ufw allow 67
+sudo ufw allow 53
+sudo ufw default allow FORWARD
 
 sudo ufw enable
